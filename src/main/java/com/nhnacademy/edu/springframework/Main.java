42463c0b9e1714f341f9e5038f59e35cb2553a71
insub2004 <insub2004@naver.com>
2024-01-29 23:10:46 +0900

    다형성 실습 1번

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,17 +1,22 @@
 package com.nhnacademy.edu.springframework;
 
+import com.nhnacademy.edu.springframework.domain.User;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        User user = new User("윤인섭@naver.com", "010-2222-2222");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Main mainInstance = new Main();
+        mainInstance.sendSmsMessage(user, " 실습 1번");
+    }
+
+    private void sendSmsMessage(User user, String message) {
+        System.out.println("SMS Message Sent to " + user.getPhoneNumber() + message);
+    }
+
+    private void sendEmailMessage(User user, String message) {
+        System.out.println("Email Message Sent to " + user.getEmail() + message);
     }
 }
