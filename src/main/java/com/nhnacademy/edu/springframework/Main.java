faed0a1c4608649158a916435b4a9047b1536db8
insub2004 <insub2004@naver.com>
2024-01-30 23:46:36 +0900

    DI 실습 01, MessageSendService 빈 객체 등록, 생성자 주입 방식으로MessageSender 타입의 빈 객체를 주입

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,9 +1,6 @@
 package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.domain.User;
-import com.nhnacademy.edu.springframework.sender.EmailMessageSender;
-import com.nhnacademy.edu.springframework.sender.MessageSender;
-import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,21 +11,11 @@ public class Main {
         User user = new User("윤인섭@naver.com", "010-2222-2222");
 
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-            //sms의 init메소드가 두 번 실행된다.
-            //email객체가 소멸될 때 destroy 메서드가 한 번 실행된다.
 
-            //scope가 prototype인 빈 객체에는 destroy 메서드를 설정해도 실행되지 않는다.
-            //그 이유는 싱클톤 스코프의 경우 생성부터 종료까지 스프링 컨테이너가 관리해주지만
-            //프로토타입의 경우 getBean이 언제 호출될지 몰라서 제어권??이 스프링 컨테이너에게 없어서 컨텍스트가 종료될 때
-            //destroy메서드를 호출해주지 못한다.
-            System.out.println("--------------");
-            new MessageSendService(context.getBean("smsMessageSender",MessageSender.class)).doSendMessage(user, "sms message");
-            System.out.println("--------------");
-            new MessageSendService(context.getBean("smsMessageSender",MessageSender.class)).doSendMessage(user, "sms message");
-            System.out.println("--------------");
-            new MessageSendService(context.getBean("emailMessageSender",MessageSender.class)).doSendMessage(user, "email message");
-            System.out.println("--------------");
-            new MessageSendService(context.getBean("emailMessageSender",MessageSender.class)).doSendMessage(user, "email message");
+            // 이제 xml에 있는 messageSender 빈 객체들을 가져와서 호출해주지 않고
+            // messageSenderService를 빈 객체로 만들고 생성자 주입 방식으로 messageSender 빈 객체를 주입해준다.
+            MessageSendService messageSendService = context.getBean("messageSendService",MessageSendService.class);
+            messageSendService.doSendMessage(user,"DI 실습01");
         }
     }
 
