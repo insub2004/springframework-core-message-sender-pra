738ad54e53f07bfe4e7fa4f42b1713b4f12b5ba0
insub2004 <insub2004@naver.com>
2024-01-31 16:10:35 +0900

    DI 실습 02, setter 주입 방식

--- src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.edu.springframework.sender;
 
 import com.nhnacademy.edu.springframework.domain.User;
+import org.springframework.context.annotation.Configuration;
 
 public class SmsMessageSender implements MessageSender{
 
--- src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
@@ -2,10 +2,19 @@ package com.nhnacademy.edu.springframework.service;
 
 import com.nhnacademy.edu.springframework.domain.User;
 import com.nhnacademy.edu.springframework.sender.MessageSender;
+import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
 
 public class MessageSendService {
 
-    private final MessageSender messageSender;
+    //final 키워드 이므로 객체를 생성한 뒤 greeter 변수에 값을 할당할 수 없다.
+    //private final MessageSender messageSender;
+
+    private MessageSender messageSender;
+
+    //기본 생성자 추가
+    public MessageSendService(){
+        System.out.println("service 기본 생성자 실행");
+    }
 
     public MessageSendService(MessageSender messageSender){
         this.messageSender = messageSender;
@@ -13,6 +22,12 @@ public class MessageSendService {
 
     public void doSendMessage(User user, String message) {
         messageSender.sendMessage(user, message);
+    }
+
+    // set추가
+    public void setMessageSender(MessageSender messageSender) {
+        System.out.println("set 메소드 실행!");
+        this.messageSender = messageSender;
     }
 
 }
