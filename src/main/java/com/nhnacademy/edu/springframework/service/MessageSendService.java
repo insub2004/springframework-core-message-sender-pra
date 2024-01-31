7d478a8e3b00535b90359c93c7e22c3644fcff21
insub2004 <insub2004@naver.com>
2024-01-31 18:31:37 +0900

    DI 실습04/02 생성자 1개인 경우 @Autowired 생략가능

--- src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
@@ -16,11 +16,10 @@ public class MessageSendService {
     private MessageSender messageSender;
 
     //기본 생성자가 무조건 있어야함 왜냐 set주입을 사용할 것이기 때문에
-    public MessageSendService(){
-        System.out.println("service 기본 생성자 실행");
-    }
-
-    @Autowired
+//    public MessageSendService(){
+//        System.out.println("service 기본 생성자 실행");
+//    }
+    
     public MessageSendService(MessageSender messageSender){
         System.out.println("파라미터 1개 있는 생성자 실행");
         this.messageSender = messageSender;
