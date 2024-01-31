22086b40485a65faa499df9f3b37ade92c580b10
insub2004 <insub2004@naver.com>
2024-01-31 21:26:47 +0900

    AnnotationConfigAppilcationContext로 변경

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -2,6 +2,7 @@ package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.domain.User;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,11 +10,12 @@ public class Main {
     public static void main(String[] args) {
         User user = new User("윤인섭@naver.com", "010-2222-2222");
 
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework.config");
+        //@Configuration 설정한 클래스
+        //@Component 설정한 클래스
 
-            MessageSendService messageSendService = context.getBean("messageSendService",MessageSendService.class);
-            messageSendService.doSendMessage(user,"DI 실습04 Autowired");
-        }
+        context.getBean("messageSendService",MessageSendService.class).doSendMessage(user,"DI 실습04 Autowired");
+
     }
 
 }
